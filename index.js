61ac3dd4198b7fc03218b4dfd8feb048422ff6f6
Lucas <lucaslopes05597@gmail.com>
2019-09-23 14:58:01 -0300

    Rotas users criada
    Criado a api para a rota users

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 
+//importando rota usuers
+const users = require('./routes/users')
 
+
+
+//rotas
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
+
+app.use('/users',users)
+
+//servidor
 
 app.listen(3000, () => console.log('SV ativo na porta 3000'));
