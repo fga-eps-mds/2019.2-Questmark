69b949a00e4ae1c2088af8dae01cb2648de7fe60
Gustavo-Nogueira <gustavonr.13@gmail.com>
2019-10-01 11:35:32 -0300

    Adição das rotas refatoradas.

---
+++ application/routes/forms.js
@@ -0,0 +1,56 @@
+
+const express = require('express');
+const router = express.Router();
+
+require('../models/Users');
+require('../models/Formulario');
+const mongoose = require('mongoose');
+const modelUsers = mongoose.model("users");
+const modelFormulario = mongoose.model("formulario");
+
+//Rota da view de registro de formulário
+router.get('/registro',(req,res)=>{
+    res.render('./formularios/cadastro_formulario');
+});
+
+//Rota para salvar formulário
+router.post('/registro/salvar',(req,res)=>{
+    let dados = req.body;
+    console.log(dados);
+    var formulario = {nome: dados.name_quest,data_quest: dados};
+    new modelFormulario(formulario).save()
+        .then(()=>{
+            console.log("Formulário salvo com sucesso!");
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+});
+
+//Rota de listagem de formulários
+router.get('/listagem',(req,res)=>{
+    modelFormulario.find().then((formulario)=>{
+        res.render("./formularios/listar_formularios",{formulario:formulario})
+    })
+})
+
+//Rota para visualizar um formulários e responder
+router.get('/visualizar/:id',(req,res)=>{
+    modelFormulario.findOne({_id:req.params.id}).then((formulario)=>{
+        res.render("./formularios/visualizar_formulario",{name_quest: formulario.nome,
+                                                          copy_html: formulario.data_quest.copy_html});
+    });
+});
+
+//Rota para apagar formulario
+router.post('/delete',(req,res)=>{
+    var id = req.body.id
+    modelFormulario.findOneAndDelete(id).then(()=>{
+        console.log('Formulário deletado.');
+    }).catch((err)=>{
+        console.log(err);
+    })
+    res.redirect('/forms/listagem');
+})
+
+module.exports = router;
