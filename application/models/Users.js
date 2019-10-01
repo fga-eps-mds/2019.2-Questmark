493beb4e43fa9d370b46efb9e8740aff522dcf7b
Gustavo-Nogueira <gustavonr.13@gmail.com>
2019-10-01 13:40:41 -0300

    Adição das models. Co-authored-by: Nicalo Ribeiro <nicalo63@gmail.com> Co-authored-by: Lucas Lopes <lucaslopes050597@gmail.com>

---
+++ application/models/Users.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+const Usuario = new Schema({
+    nome:{
+        type: String,
+        required: true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    senha:{
+        type: String,
+        required: true
+    },
+    admin:{
+        type: Number,
+        required: true,
+        default: 0
+    },
+    formulario:{
+        type: Schema.Types.ObjectId,
+        ref:"formulario",
+        required: false	
+    }
+})
+
+module.exports = mongoose.model('users', Usuario);
+
+
+
