75a33edc5320ddb8211124c5bf6664d425379d5f
Lucas <lucaslopes05597@gmail.com>
2019-10-04 17:47:32 -0300

    [#49] Teste criar um formulario [Aprovado]
    Co-Authored-By: Miguel <miguelisdemir@gmail.com>

--- __tests__/bancoDados.test.js
+++ __tests__/bancoDados.test.js
@@ -1,5 +1,10 @@
 //importando o mongo
 const mongoose = require('mongoose');
+//importando o model
+const modelFormulario = require('../models/Formulario');
+//criando um dado do tpo formulario com todos os campos
+const formData = { nome: 'Lucas', data_quest: [{name_quest: 'Lucas'}], respostas: [ { nome: 'Lucas Lopes', idade: '20' } ], data: new Date()
+};
 
 
 //conectar com o mongo
@@ -13,6 +18,29 @@ describe('Conectar com o  mongo', () => {
             }
         });
 });
+
+
+//teste para criar e salvar um formulario
+
+it('Criar e salvar um formulario generico', async (done) => {
+    //criando um formulario
+    const formulario = new modelFormulario(formData);
+    //salvando
+    const salvarformulario = await formulario.save();
+    //verificando se o id está definido
+    expect(salvarformulario._id).toBeDefined();
+    //verificando se é igual
+    expect(salvarformulario.nome).toBe(formData.nome);
+     //verificando se é igual
+    expect(salvarformulario.data_quest).toBe(formData.data_quest);
+     //verificando se é igual
+    expect(salvarformulario.data).toBe(formData.data);
+     //verificando se é igual
+    expect(salvarformulario.respostas).toBe(formData.respostas);
+    done ();
+});
+
+
 
 
 
