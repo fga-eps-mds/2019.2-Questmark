e043eb91bb98ac8732700a04a9a2fa73d185baa1
Nícalo Ribeiro <nicalo63@gmail.com>
2019-10-07 17:05:27 -0300

    [#48] Componente header criado.
    Co-Authored-by: Luiz Gustavo <lgpinheiro94@gmail.com>

---
+++ client/questmark/src/components/Header.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+export default class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <header className="mainHeader">
+                Questmark
+                <div className="options">
+                    <select name="ordernar">
+                    <option value="" selected>Ordenar por :</option>
+                        <option value="alfabetica">Ordem Alfabética [A-Z]</option>
+                        <option value="criacao" selected>Data de criação</option>
+                        <option value="edicao">Data de modificação</option>
+                    </select>
+                    <p>Ajuda</p>
+                    <p>Opções</p>
+                    <p>Log Out</p>
+                </div>
+            </header>
+
+        )
+    }
+
+}
