d4b2702d97aa2a5543ff6d68a9fe297627c005ec
miguelisdemir <miguelisdemir@gmail.com>
2019-10-07 22:44:31 -0300

    rota get

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,18 @@
 
 const express = require('express')
 const router = express.Router()
-require('../models/Formulario')
 const mongoose = require('mongoose')
-const modelFormulario = mongoose.model("formulario")
 
 require('../models/Users')
 const modelUsers = mongoose.model("users")
+
+
+const passport = require('passport')
+
+
+
 router.get('/',(req,res)=>{
     res.render('./usuarios/criarconta')
 })
-
-router.post('/criarconta',(req,res)=>{
-    var usuario = {
-        nome : req.body.nome,
-        email: req.body.email,
-        senha: req.body.senha
-    }  
-    console.log(usuario)
-    new modelUsers(usuario).save().then(()=>{
-        console.log("salvo com sucesso")
-        res.redirect('/forms')
-    }).catch((err)=>{
-        console.log(err)
-        res.redirect('/')
-    })
-})
-
 
 module.exports = router
