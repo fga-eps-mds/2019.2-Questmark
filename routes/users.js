6d2db94ebae7ff580b752c48aec82e51c2e07f8d
miguelisdemir <miguelisdemir@gmail.com>
2019-10-07 22:53:22 -0300

    rotas criar conta e login

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
@@ -13,6 +12,44 @@ const passport = require('passport')
 
 router.get('/',(req,res)=>{
     res.render('./usuarios/criarconta')
+})
+
+router.post('/criarconta',(req,res)=>{
+
+   modelUsers.findOne({email:req.body.email}).then((usuario)=>{
+       if(usuario){
+           console.log("usuario já existe")
+           res.redirect('/users')
+       }
+       else{
+            const novousuario = new modelUsers({
+                nome: req.body.nome,
+                email: req.body.email,
+                senha:req.body.senha,
+            })
+            novousuario.save().then(()=>{
+                console.log("Salvou")
+            }).catch((erro)=>{
+                console.log(erro)
+            })
+
+           //hash de criptografa 
+       }
+   })
+
+
+})
+
+router.get('/login',(req,res)=>{
+    res.render('./usuarios/login')
+})
+
+router.post('/login',(req,res,next)=>{
+    passport.authenticate("local",{
+        successRedirect: '/forms',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req,res,next)
 })
 
 module.exports = router
