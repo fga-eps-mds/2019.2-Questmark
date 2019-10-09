01d387a82f1d8d07d5074159e3795c4a13cc70fd
Nícalo Ribeiro <nicalo63@gmail.com>
2019-10-08 22:20:02 -0300

    [#48] Botoes adicionados e input para nome do formulario ajustado.
    Co-Authored-by: Luiz Gustavo <lgpinheiro94@gmail.com>

--- client/questmark/src/pages/CreateForm.js
+++ client/questmark/src/pages/CreateForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { Button } from '@material-ui/core';
 import './CreateForm.css';
 
 
@@ -20,28 +21,28 @@ export default class CreateForm extends Component {
                 <Header />
                 <div className="formContainer">
                     <form id="form_registro">
-                        <div>
+                        <div className="formName">
                             <label for='name_form'>Nome do formulário :</label>
-
-                            <input type="text" id="name_quest" name="name_quest" placeholder=" Nome do Questionário"
-                                class="textBoxName" />
+                            <input type="text" id="name_quest" name="name_quest" placeholder=" Digite aqui..."/>
                         </div>
                         <div className="formsField">
                             <div className="inputMD">
-                                <textarea id="notes" name="copy_markdown" placeholder="Formulário em Markdown" class="textBox"></textarea>
+                                <textarea id="notes" name="copy_markdown" placeholder="# Escreva aqui o formulário em Markdown" class="textBox"></textarea>
                             </div>
                             <div >
                                 <div class="textBox" id="notes-preview"></div>
                             </div>
                             <div className="outputHTML">
-                                <textarea type="text" id="copy_html" name="copy_html" class="form-control" placeholder="Saída em HTML">
+                                <textarea type="text" id="copy_html" name="copy_html" class="form-control" placeholder="# Saída do formulário">
                                 </textarea>
                             </div>
-                        </div>
-
-
+                        </div>                        
                     </form>
-
+                    <div className="buttons">
+                    <Button variant={'contained'} size={'large'} color={"primary"}>Voltar</Button>
+                    <Button variant={'contained'} size={'large'} color={"primary"}>Criar</Button>
+                    
+                    </div>
                 </div>
             </div>
 
