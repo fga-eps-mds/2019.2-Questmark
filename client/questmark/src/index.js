45450931424e9e453a032873c55c925d818b3973
Lucas <lucaslopes05597@gmail.com>
2019-10-11 01:04:35 -0300

    [#61] Organizando pastas do github
    Co-Authored-By: Luiz <lgpinheiro94@gmail.com>

--- client/questmark/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import CreateForm from './pages/CreateForm';
-import ListForms from './pages/ListForms';
-
-ReactDOM.render(<ListForms />, document.getElementById('root'));
-
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var app = require('./config/server');
+var app = require('./backend/config/server');
 
 app.listen(3000,function(){
 	console.log('Servidor ON');
