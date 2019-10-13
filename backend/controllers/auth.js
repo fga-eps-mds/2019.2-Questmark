1f6ffdddfc7cf545200e447a1817b45ec08aa81d
miguelisdemir <miguelisdemir@gmail.com>
2019-10-12 21:02:21 -0300

    [#64] Comparação das senhas criptografadas para login. Co-Authored-by: Gustavo Nogueira<gustavonr.13@gmail.com>

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,35 +1,38 @@
-const local = require('passport-local').Strategy
-const mongoose = require('mongoose')
-//model de usuarios
-require('../models/Users')
-const modelUsuario = mongoose.model('users')
-
-
+const local = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+require('../models/Users');
+const modelUsuario = mongoose.model('users');
+const crypto = require('crypto');
 
 module.exports = function(passport){
     //criando uma sessão 
     passport.use(new local({usernameField: 'email',passwordField: 'senha'},(email,senha,done)=>{
         modelUsuario.findOne({email:email}).then((usuario)=>{
+            //Verificação dos dados de email no bd
             if(!usuario){
-                return done (null,false,{message:"Esta conta não existe"})
+                return done(null,false,{message:"Usuário não existe."});
             }else{
-               
-                return done(null,usuario)
-            
+                
+                //Verificação da senha
+                const senhaCrypto = crypto.createHash('md5').update(senha).digest('hex');
+                if(usuario.senha === senhaCrypto){
+                    return done(null,usuario);
+                }
+                else{
+                    return done(null,false,{message:"A senha inserida está incorreta."});  
+                }
             }
-
-            //{}criptografia para comparar senhas
-        })
+        });
         
-    }))
+    }));
     //salva os dados do usuario em uma sessão
     passport.serializeUser((usuario,done)=>{
         done(null,usuario.id)
-    })
+    });
     //procura um usuario pelo id dele
     passport.deserializeUser((id,done)=>{
         modelUsuario.findById(id,(err,usuario)=>{
             done(err,usuario)
-        })
-    })
+        });
+    });
  }
