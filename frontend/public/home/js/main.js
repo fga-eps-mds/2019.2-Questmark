632d4a3838b255b2ea6060310e5eacb0755f6cd9
Luiz Gustavo <lgpinheiro94@gmail.com>
2019-10-15 22:56:39 -0300

    [#66] Adicionando o arquivo js e as libs. Co-Authored-by: Lucas <lucaslopes05597@gmail.com>

---
+++ frontend/public/home/js/main.js
@@ -0,0 +1,36 @@
+(function ($) {
+  "use strict";
+
+ // iniciando as animações wow
+  new WOW().init();
+
+
+  // animação do botão  topo
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+
+ 
+  // Roda o cabeçalho na pagina
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+})(jQuery);
+
