c5dcf4061de6529e45ad7072e728194f90134cbc
Gustavo-Nogueira <gustavonr.13@gmail.com>
2019-10-19 21:21:07 -0300

    [#81]Adição de ícones para feedback visual.
    Co-Authored-by: Nicalo Ribeiro <nicalo63@gmail.com>

--- frontend/public/login_e_cadastro/javascript/form_validation.js
+++ frontend/public/login_e_cadastro/javascript/form_validation.js
@@ -5,16 +5,24 @@ function passwordValidation(email){
 function emailValidation(email){
 	let url_req = '/users/check_email';
 	let json = {email};
-	if(email !== ''){
+	let icon = document.getElementById('iconEmail');
+	if(email !== '' && email.indexOf('@') != -1){
 		$.post(url_req,json,(resp) => {
 			if(resp.check){
 				//OK
+				icon.src = '/login_e_cadastro/images/check.png';
+				icon.style.visibility = 'visible';
 				console.log('Ok!');
 			}
 			else{
 				//Email já utilizado
+				icon.src = '/login_e_cadastro/images/error.png';
+				icon.style.visibility = 'visible';
 				console.log('Usuário já existe!');	
 			}
 		});	
+	}
+	else{
+		icon.style.visibility = 'hidden';
 	}
 }
