3042d193625b752914a80b0b44717fba5cbb5ce6
Nícalo Ribeiro <nicalo63@gmail.com>
2019-10-22 20:04:07 -0300

    [#75] Adicionada logica na rota para redefinir a senha a partir do token criado.
    Co-Authored-by: Gustavo Nogueira <gustavonr.13@gmail.com>

--- backend/routes/forgot_password.js
+++ backend/routes/forgot_password.js
@@ -32,6 +32,34 @@ router.post('/recuperar_senha', async (req, res) => {
     };
 });
 
-router.post('/redefinir_senha', async (req, res) => { });
+router.post('/redefinir_senha', async (req, res) => {
+
+    const { email, token, senha } = req.body;
+
+    try {
+        const user = await User.findOne({ email })
+            .select('+passwordResetToken passwordResetExpires');
+        if (!user) {
+            return res.status(400).send({ error: 'Usuário não encontrado !' });
+        }
+        if (token !== user.passwordResetToken) { //Verifica se o token é válido para o usuário
+            return res.status(400).send({ error: 'Token inválido!' });
+        }
+        const now = new Date(); //Veririfica se o token já expirou
+        if (now > user.passwordResetExpires) {
+            res.status(400).send({ error: 'Token expirou!' });
+        }
+        const senhaCrypto = await crypto.createHash('md5').update(senha).digest('hex'); //Encripta a nova senha
+
+        user.senha = senhaCrypto;
+
+        await user.save();
+
+        res.send()
+
+    } catch (error) {
+        res.status(400).send({ error: 'Erro ao redefinir senha.' })
+    }
+});
 
 module.exports = router;
