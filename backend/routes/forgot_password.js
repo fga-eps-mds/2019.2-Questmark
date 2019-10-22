dc3bd5bc71e8f68622c2c6ad18cfed385a88b190
Nícalo Ribeiro <nicalo63@gmail.com>
2019-10-22 19:42:42 -0300

    [#75]Adicionando logica para a criacao do token que, futuramente, sera enviado para o email para a recuperacao de senha.
    Co-Authored-by: Gustavo Nogueira <gustavonr.13@gmail.com>

--- backend/routes/forgot_password.js
+++ backend/routes/forgot_password.js
@@ -4,8 +4,34 @@ const User = require('../models/Users');
 
 const router = express.Router();
 
-router.post('/recuperar_senha', async (req, res) => {});
+router.post('/recuperar_senha', async (req, res) => {
+    const { email } = req.body;
+    try {
 
-router.post('/redefinir_senha', async (req, res) => {});
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).send({ error: 'Usuário não encontrado !' });
+        }
+
+        const token = crypto.randomBytes(20).toString('hex');  // Cria o token
+
+        const now = new Date();
+        now.setHours(now.getHours() + 1); //Define o tempo em que o token ficará ativo ( 1 hora )
+
+        await User.findByIdAndUpdate(user.id, {
+            '$set': {
+                passwordResetToken: token,
+                passwordResetExpires: now,
+            }
+        });
+
+        console.log(token, now);
+
+    } catch (error) {
+        res.status(400).send({ error: 'Erro ao tentar recuperar senha.' })
+    };
+});
+
+router.post('/redefinir_senha', async (req, res) => { });
 
 module.exports = router;
