7db369dcbf817dca0cd7c4fd91c9830da3429d10
Gustavo-Nogueira <gustavonr.13@gmail.com>
2019-10-27 21:32:36 -0300

    [#86] Refatoração para correção de bugs do script de estilização de formulários.
    Co-Authored-By: Lucas Lopes <lucaslopes05597@gmail.com>

--- frontend/public/exibicao_formulario/javascript/input_field_stylization.js
+++ frontend/public/exibicao_formulario/javascript/input_field_stylization.js
@@ -1,74 +1,44 @@
-	
-function stylizeFields(){
 
-	let iRadio = Array(),iCheckbox = Array();
-	//Tag Input
-	let inputElements = document.getElementsByTagName('input');
-	for(let i = 0;i < inputElements.length;i++){
-		if(inputElements[i].type == 'checkbox'){	
-			makeInputContext(iCheckbox,inputElements[i]);
-		}else if(inputElements[i].type == 'radio'){
-			makeInputContext(iRadio,inputElements[i]);
-		}else if(inputElements[i].type == 'text'){
-			inputElements[i].className = 'form-control';
+function apllyClass(arrayElements,classBootstrap){
+	let visited_fields = Array();
+	arrayElements.forEach((element) => {
+		if(visited_fields.indexOf(element.name) == -1){
+			let divParent = document.getElementById(`${element.name}`);	
+			divParent.className = 'field mx-sm-2';
+			visited_fields.push(element.name);
 		}
-	}
-
-	//Tag Select
-	let selectElements = document.getElementsByTagName('select');
-	for(let i = 0;i < selectElements.length;i++){
-		selectElements[i].className = 'form-control';
-	}
-					
-	iRadio.forEach(function(cjtRadio){
-		//console.log(cjtRadio.parentAll);
-		//cjtRadio.parent.className = 'custom-control custom-radio';
-		cjtRadio.parent.className = 'custom-control custom-radio custom-control-inline';
-		cjtRadio.child_input.className = 'custom-control-input';
-		cjtRadio.child_label.className = 'custom-control-label';
-		cjtRadio.parent.appendChild(cjtRadio.child_input);
-		cjtRadio.parent.appendChild(cjtRadio.child_label);
-						
-		//CSS
-		cjtRadio.child_label.style.marginLeft = '5px';
-		cjtRadio.parentAll.style.paddingLeft = '5px';
-		cjtRadio.parentAll.style.paddingTop= '20px	';
-		cjtRadio.parentAll.style.marginBottom= '10px';
-		cjtRadio.parentAll.style.border = '1.5px solid rgba(35,152,214,0.8)';
-		cjtRadio.parentAll.style.borderRadius = '10px';
+		let labelOption = document.getElementById(`lbl-${element.option}`);
+		let divOption = document.getElementById(`div-${element.option}`);
+		let inputOption = document.getElementById(`${element.option}`);
+		divOption.className = classBootstrap;
+		inputOption.className = 'custom-control-input';
+		labelOption.className = 'custom-control-label labelOption';
 	});
-
-	iCheckbox.forEach(function(cjtCheckbox){
-		//console.log(cjtCheckbox.parentAll);
-		cjtCheckbox.parent.className = 'custom-control custom-checkbox';
-		cjtCheckbox.child_input.className = 'custom-control-input';
-		cjtCheckbox.child_label.className = 'custom-control-label';
-		cjtCheckbox.parent.appendChild(cjtCheckbox.child_input);
-		cjtCheckbox.parent.appendChild(cjtCheckbox.child_label);
-						
-		//CSS
-		cjtCheckbox.parentAll.style.paddingLeft = '5px';
-		cjtCheckbox.parentAll.style.paddingTop = '20px';
-		cjtCheckbox.parent.style.display = 'inline';
-		cjtCheckbox.child_label.style.marginLeft = '5px';
-		cjtCheckbox.parent.style.display = 'inline';
-		cjtCheckbox.parentAll.style.border = '1.5px solid rgba(35,152,214,0.8)';
-		cjtCheckbox.parentAll.style.borderRadius = '10px';
-		cjtCheckbox.parentAll.style.marginBottom= '10px';
-	});
-
 }
 
-function makeInputContext(arrayIContext,element){
-	let label,labels = element.parentNode.getElementsByTagName('label');
-					
-	let divParent = document.createElement('div');
-	element.parentNode.appendChild(divParent);
-						
-	for(let j = 0;j < labels.length;j++){
-		if(element.value == labels[j].htmlFor) {label = labels[j];break;}
-	}
+function stylizeFields(dataInputs){
+	let classBootstrap;
 
-	arrayIContext.push({parent: divParent, child_input: element,child_label: label,
-						parentAll: element.parentNode.parentNode});
+	//Checkbox
+	classBootstrap = 'custom-control custom-checkbox optionField';
+	apllyClass(dataInputs.checkbox,classBootstrap);
+
+	//Radio
+	classBootstrap = 'custom-control custom-radio custom-control-inline optionField';
+	apllyClass(dataInputs.radio,classBootstrap);
+
+	//Text
+	dataInputs.text.forEach((element) => {
+		let domElement = document.getElementById(element.name);
+		domElement.parentNode.className = 'form-inline mx-sm-2';
+		domElement.className = 'form-control mx-sm-2';
+	});
+
+	//Select
+	dataInputs.select.forEach((element) => {
+		// document.getElementById(element.name).className = 'form-control';
+		let domElement = document.getElementById(element.name);
+		domElement.parentNode.className = 'form-inline mx-sm-2';
+		domElement.className = 'form-control mx-sm-2';			
+	});
 }
