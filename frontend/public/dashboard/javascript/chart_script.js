5e463bc7d7b1afed34053bf64756760d4680cc35
miguelisdemir <miguelisdemir@gmail.com>
2019-11-04 16:35:07 -0300

    [#78]Criação do histograma de frequencia das respostas.
    Co-Authored-by: Nicalo Ribeiro <nicalo63@gmail.com>

--- frontend/public/dashboard/javascript/chart_script.js
+++ frontend/public/dashboard/javascript/chart_script.js
@@ -11,3 +11,45 @@ function generateColors(size) {
     }
     return colors;
 }
+
+//Inicializa o histograma com 0.
+function initializeFieldHistogram(mapField,histogramFields) {
+    if(!mapField) return;
+    mapField.forEach((element) => {
+        if(histogramFields[element.name] === undefined){
+                histogramFields[element.name] = Array();
+        }
+        histogramFields[element.name][element.option] = 0;
+    });
+}
+
+//Gera o histograma de respostas para cada campo.
+function generateHistogram(jsonAnswers,fieldMapping) {
+    //Criando o array de histograma
+    let histogramFields = Array();
+    //Checkbox
+    initializeFieldHistogram(fieldMapping.checkbox,histogramFields);
+    //Radio
+    initializeFieldHistogram(fieldMapping.radio,histogramFields);
+    //Select
+    initializeFieldHistogram(fieldMapping.select,histogramFields);
+
+    //Contabilizando respostas no array de histograma
+    jsonAnswers.forEach((element) => {
+        for(let field in histogramFields){
+            if(Array.isArray(element[field])){
+                element[field].forEach((answer) => {
+                    histogramFields[field][answer]++;
+                });
+            }
+            else{
+                histogramFields[field][element[field]]++;
+            }
+        }
+    });
+
+    console.log(jsonAnswers);
+    console.log(histogramFields);
+
+    return histogramFields;
+}
