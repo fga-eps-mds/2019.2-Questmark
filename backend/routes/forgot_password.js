1294918942d7f04acc53e9cd766e83f4e77b817d
Nícalo Ribeiro <nicalo63@gmail.com>
2019-11-05 23:24:54 -0200

    [#94] Retirando KEY da API e criando arquivos CSS para seus respectivos EJS.

--- backend/routes/forgot_password.js
+++ backend/routes/forgot_password.js
@@ -4,7 +4,7 @@ const User = require('../models/Users');
 //const mailer = require('../modules/mailer');
 const sgMail = require('@sendgrid/mail');
 const router = express.Router();
-sgMail.setApiKey("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");
+sgMail.setApiKey("COLOCAR KEY AQUI");
 
 router.get('/forgot', (req, res) => {
     res.render('./usuarios/solicitar_nova_senha', { status: false });
