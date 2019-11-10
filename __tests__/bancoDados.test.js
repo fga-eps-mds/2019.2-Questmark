67d049cdb38e78ef3e0e48d6a6a486a80db37941
miguelisdemir <miguelisdemir@gmail.com>
2019-11-10 01:09:37 -0300

    Refatorando teste do banco de dados para usuarios e formularios.
    Co-Authored-By:Lucas Lopes<lucaslopes05597@gmail.com>

--- __tests__/bancoDados.test.js
+++ __tests__/bancoDados.test.js
@@ -28,7 +28,6 @@ describe('Conectar com o  mongo', () => {
 
 
 //teste para criar e salvar um formulario
-
 it('Deve criar e salvar um formulario generico', async () => {
     //criando um formulario
     const formulario = new modelFormulario(formData);
@@ -45,5 +44,86 @@ it('Deve criar e salvar um formulario generico', async () => {
      //verificando se é igual
     expect(salvarformulario.respostas).toBe(formData.respostas);
 });
+
+//teste para inserir formulario com campo inexistente e verificar se esse campo é indefinidado
+it('Não deve inserir formulario  com um campo  inexistente.', async () => {
+    //criando um formulario
+    const formulariocampoinvalido = new modelFormulario({ nome: 'Lucas', data_quest: [{name_quest: 'Lucas'}], data: new Date(),resp:'a'});
+    //salvando o formulario
+    const salvarformulariocampoinvalido = await formulariocampoinvalido.save();
+    //verificando se é definido (existe)
+    expect(salvarformulariocampoinvalido._id).toBeDefined();
+    //verificar se o campo .resp é indefinido 
+    expect(salvarformulariocampoinvalido.resp).toBeUndefined();
+});
+    //'criando um formulario sem um campo obrigatorio e verificado a falha'
+it('Não deve criar sem um campo obrigatorio ', async () => {
+    //criando formulario so com o nome
+    const formularioSemObrigatorio = new modelFormulario({ nome: 'Lucas' });
+    let err;
+    try {
+        //tenta salvar
+        const salvarFormularioSemObrigatorio = await formularioSemObrigatorio.save();
+        error = salvarFormularioSemObrigatorio;
+    } catch (error) {
+        //salva o erro
+        err = error
+    }
+    expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
+    //verifica que esse campo tinha que ser definido
+    expect(err.errors.data_quest).toBeDefined();
+});
+
+it('Deve Encontrar o formulario cadastrado',async()=>{
+    const system = await modelFormulario.findOne({ nome: 'Lucas' });
+    expect(system.nome).toEqual(formData.nome);
+    expect(system.nome).toBeDefined();
+})
+
+it('Deve criar um usuario génerico',async()=>{
+    //criando um usuário
+    const usuario = new modelUsers(userdata);
+    const salvarusuario = await usuario.save();
+    expect(salvarusuario._id).toBeDefined();
+    expect(salvarusuario.nome).toBe(userdata.nome);
+    expect(salvarusuario.email).toBe(userdata.email);
+    expect(salvarusuario.senha).toBe(userdata.senha);
+})
+
+it('Não deve inserir usuario  com um campo  inexistente.',async () =>{
+    //criando um usuario
+    var invalidmodel = { nome: 'Lucas',email: 'lb@gmail.com',senha:"lucas123",telefone:'0000000000'};
+    const userinvalido = new modelUsers(invalidmodel);
+    //salvando o formulario
+    const salvaruserinvalido = await userinvalido.save();
+    //verificando se é definido (existe)
+    expect(salvaruserinvalido._id).toBeDefined();
+    //verificar se o campo telefone é indefinido 
+    expect(salvaruserinvalido.telefone).toBeUndefined();
+});
+
+it('Não deve criar  um usuario sem um campo obrigatorio ', async () => {
+    //criando formulario so com o nome
+    var userinvalid = { nome: 'Lucas', email:'lg@gmail.com'}
+    const usuariosemcampo = new modelFormulario(userinvalid);
+    let err;
+    try {
+        //tenta salvar
+        const salvaruserinvalido = await usuariosemcampo.save();
+        error = salvaruserinvalido;
+    } catch (error) {
+        //salva o erro
+        err = error
+    }
+    expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
+    //verifica que esse campo tinha que ser definido
+    expect(err.errors.senha).toBeUndefined();
+});
+
+it('Deve Encontrar o usuario cadastrado no banco',async()=>{
+    const systemusers = await modelFormulario.findOne({ nome: 'Lucas' });
+    expect(systemusers.nome).toEqual(userdata.nome);
+    expect(systemusers.nome).toBeDefined();
+})
 
 })
