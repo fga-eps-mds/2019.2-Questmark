7a0e41446e0f20fa0f2610728639b8033fe7e478
Danillo Souza <danillosouza1704@gmail.com>
2019-11-16 20:46:56 -0200

    #120 - Adicionando linhas nulas ao CSV
    Co-Authored-by: Nicalo Ribeiro <nicalo63@gmail.com>

--- backend/controllers/csv.js
+++ backend/controllers/csv.js
@@ -1,8 +1,22 @@
 module.exports = function arrayToCSV(objArray) { {
-    const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
-    let str = `${Object.keys(array[0]).map(value => `"${value}"`).join(";")}` + '\r\n';
+    let tagTotal = Object.keys(objArray.data_quest.type_inputs).length;
+    const array = typeof objArray !== 'object' ? JSON.parse(objArray.respostas) : objArray.respostas;
+    const totalKey = array.find(array => Object.keys(array).length > tagTotal);
+    let str = `${Object.keys(totalKey).map(value => `"${value}"`).join(";")}` + '\r\n';
     return array.reduce((str, next) => {
-        str += `${Object.values(next).map(value => `"${value}"`).join(";")}` + '\r\n';
+        if(Object.keys(next).map(value => value).length < tagTotal){
+            console.log();
+            let index = Object.keys(totalKey).findIndex(index => index == Object.keys(next));
+            console.log(index);
+            console.log(next);
+            if( index < 0){
+                str += `${Object.values(totalKey).map(() => null).join(";")}` + '\r\n';
+            }
+            // else
+            //     str += `${Object.values(next[index]).map(() => null).join(";")}` + '\r\n';
+        }
+        else
+            str += `${Object.values(next).map(value => `"${value}"`).join(";")}` + '\r\n';
         return str;
     }, str);
 } 
