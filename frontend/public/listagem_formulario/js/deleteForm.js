00e72f3f15d754bed4438d359cf4b05f4b3306cd
Danillo Souza <danillosouza1704@gmail.com>
2019-11-15 23:58:31 -0200

    [#118] - Adicionando overlay de deletar o formulario
    CoAuthored-by: Nicalo Ribeiro <nicalo63@gmail.com>

--- frontend/public/listagem_formulario/js/deleteForm.js
+++ frontend/public/listagem_formulario/js/deleteForm.js
@@ -1,5 +1,5 @@
 deleteForm = (formId, formName) => {
-    document.getElementById("DeleteModalInterno").innerHTML = formName;
+    document.getElementById("ModalInternoDelete").innerHTML = formName;
     document.getElementById("delete").href = '/forms/delete/'+formId;
     console.log('/forms/delete/'+formId);   
 }
