6bacb292660d13f66886b966e642dcbf3ca2899e
Nícalo Ribeiro <nicalo63@gmail.com>
2019-11-16 18:07:37 -0200

    [#116] Retirada dos comentarios e adicao das arrow functions nos arquivos do frontend.
    Co-Authored-By: Danillo <danillosouza1704@gmail.com>

--- frontend/public/listagem_formulario/js/copyForm.js
+++ frontend/public/listagem_formulario/js/copyForm.js
@@ -1,10 +1,9 @@
 getInfo = (formId, formName) => {
     document.getElementById("ModalInterno").innerHTML = formName;
     document.getElementById("shareLink").value = 'http://localhost:3000/forms/postar/' + formId;
-    document.getElementById("iFrame").value = '<iframe src=' + 
-                                              '"http://localhost:3000/forms/postar/' + formId + '"' +
-                                              ' height="300px" width="60%"' + '></iframe>';
-    
+    document.getElementById("iFrame").value = '<iframe src=' +
+        '"http://localhost:3000/forms/postar/' + formId + '"' +
+        ' height="300px" width="60%"' + '></iframe>';
 }
 copyLink = () => {
     var copyText = document.getElementById("shareLink");
