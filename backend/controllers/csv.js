8a3fb68d95ff92ee1fe308f89211840d32388efb
Danillo Souza <danillosouza1704@gmail.com>
2019-11-18 19:03:36 -0200

    [#120] - Permitindo a criação de CSV bem formatados
    Co-Authored-by: Nicalo Ribeiro <nicalo63@gmail.com>

--- backend/controllers/csv.js
+++ backend/controllers/csv.js
@@ -1,24 +1,26 @@
 module.exports = function arrayToCSV(objArray) { {
-    let tagTotal = Object.keys(objArray.data_quest.type_inputs).length;
-    const array = typeof objArray !== 'object' ? JSON.parse(objArray.respostas) : objArray.respostas;
-    const totalKey = array.find(array => Object.keys(array).length > tagTotal);
-    let str = `${Object.keys(totalKey).map(value => `"${value}"`).join(";")}` + '\r\n';
-    return array.reduce((str, next) => {
-        if(Object.keys(next).map(value => value).length < tagTotal){
-            console.log();
-            let index = Object.keys(totalKey).findIndex(index => index == Object.keys(next));
-            console.log(index);
-            console.log(next);
-            if( index < 0){
-                str += `${Object.values(totalKey).map(() => null).join(";")}` + '\r\n';
+    let array = typeof objArray !== 'object' ? JSON.parse(objArray.respostas) : objArray.respostas;
+    let titles = objArray.data_quest.type_inputs.fields;
+    let str = `${Object.values(titles).map(value => `"${value}"`).join(";")}` + '\r\n';
+    return array.reduce((str, next) => {  
+        if(Object.keys(next).length != titles.length){
+            if(Object.keys(next).length == 0)
+                str += `${Object.values(next).map(() => "\t").join(";")}` + '\r\n';
+            else {
+                for (let i = 0, j = 0; i < titles.length; i++) {
+                    if(titles[i] == Object.keys(next)[j]){
+                        str += Object.values(next)[j] + ";";
+                        j++;
+                    }
+                    else
+                        str += "\t" + ";";
+                }
+                str += '\r\n';
             }
-            // else
-            //     str += `${Object.values(next[index]).map(() => null).join(";")}` + '\r\n';
         }
         else
             str += `${Object.values(next).map(value => `"${value}"`).join(";")}` + '\r\n';
         return str;
     }, str);
 } 
-
 }
