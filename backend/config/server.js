12e031d1e1355a0f7953efea6477dcd6241f5a86
Luiz Gustavo <lgpinheiro94@gmail.com>
2019-11-20 13:22:17 -0300

    [#121]Mudança dos links que funcionam localmente para funcionar de maneira dinamica.

--- backend/config/server.js
+++ backend/config/server.js
@@ -39,7 +39,7 @@ app.use((req, res, next)=>{
 });
 
 //Conexão do MongoDB
-const url = "mongodb+srv://questmarkdeploy:questmarkdeploy@cluster0-62j7g.mongodb.net/test?retryWrites=true&w=majority";
+const url = "mongodb://mongo/questmark"
 mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 	.then(() => {
 		console.log('Conectado com sucesso ao banco de dados.');
